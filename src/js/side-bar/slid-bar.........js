9ccb7680c5dcd506701eaa1ca8eadf2e62cb4975
Behrouz <behrouzshakeri05@gmail.com>
2025-11-19 19:55:05 +0330

    Fix Sidebar Bug

--- src/js/side-bar/slid-bar.........js
+++ src/js/side-bar/slid-bar.........js
@@ -1,7 +1,16 @@
 document
-  .querySelector(".side-bar__open-close-btn-icon")
+  .querySelector(".side-bar__open-btn-icon")
   .addEventListener("click", function (event) {
-    document.querySelector(".side-bar").classList.toggle("side-bar--close");
-    // TODO: chang methot to best
-    document.querySelector(".app").classList.toggle(".app--full");
+    document.querySelector(".app").style.marginRight = "32rem";
+    document.querySelector(".side-bar").style.width = "24vw";
+    document.querySelector(".side-bar").style.display = "block";
+    document.querySelector(".side-bar__open-btn-icon").style.display = "none";
+  });
+document
+  .querySelector(".side-bar__close-btn-icon")
+  .addEventListener("click", function (event) {
+    document.querySelector(".app").style.marginRight = "0%";
+    document.querySelector(".side-bar").style.display = "none";
+    document.querySelector(".side-bar__open-btn-icon").style.display =
+      "inline-block";
   });
