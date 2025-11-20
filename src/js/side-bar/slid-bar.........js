1c758410f4f713ed5c0440ff900fd3c3df0bb7eb
Behrouz <behrouzshakeri05@gmail.com>
2025-11-20 11:44:01 +0330

    responsive sidebar

--- src/js/side-bar/slid-bar.........js
+++ src/js/side-bar/slid-bar.........js
@@ -1,16 +1,16 @@
 document
   .querySelector(".side-bar__open-btn-icon")
   .addEventListener("click", function (event) {
-    document.querySelector(".app").style.marginRight = "32rem";
-    document.querySelector(".side-bar").style.width = "24vw";
-    document.querySelector(".side-bar").style.display = "block";
+    // document.querySelector(".app").style.marginRight = "40rem";
+    document.querySelector(".app").classList.toggle("app--open");
+    document.querySelector(".side-bar").classList.toggle("side-bar--open");
     document.querySelector(".side-bar__open-btn-icon").style.display = "none";
   });
 document
   .querySelector(".side-bar__close-btn-icon")
   .addEventListener("click", function (event) {
-    document.querySelector(".app").style.marginRight = "0%";
-    document.querySelector(".side-bar").style.display = "none";
+    document.querySelector(".app").classList.toggle("app--open");
+    document.querySelector(".side-bar").classList.toggle("side-bar--open");
     document.querySelector(".side-bar__open-btn-icon").style.display =
       "inline-block";
   });
